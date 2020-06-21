3140474a1e764e3fd9e3f5bddc9aaaa0774caf2e
mark <dev.kram5@gmail.com>
2020-06-21 22:14:19 +0800

    Remove symbolink link and updated .gitignore

--- lib/random.h
+++ lib/random.h
@@ -1,7 +1,7 @@
 #ifndef petipa_mocklib_rng_h_included
 #define petipa_mocklib_rng_h_included
 
-#include <petipa/api/editor.h>
+#include "../include/editor.h"
 namespace ed = petipa::api::editor;
 ed::VisualizationOptions::DisplayAll random_display_all();
 ed::VisualizationOptions::PathDisplayParts random_path_display_parts();
