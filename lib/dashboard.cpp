2924e82ded170827851a32b0cffb38b54ff44d07
Rodolfo Borges <rodolfo.borges@gmail.com>
2020-08-25 13:25:07 -0300

    rename abbreviation db->dash

--- lib/dashboard.cpp
+++ lib/dashboard.cpp
@@ -4,9 +4,9 @@
 #include "../include/dashboard.h"
 #include "run.h"
 #include "util.h"
-namespace db = petipa::api::dashboard;
+namespace dash = petipa::api::dashboard;
 
-static std::vector<db::Project> projects =
+static std::vector<dash::Project> projects =
 {
 	{ "Um sonho", "há poucos minutos", "19:02" },
 	{ "Dança de fim de ano", "anteontem", "8:40" },
@@ -14,15 +14,15 @@ static std::vector<db::Project> projects =
 	{ "Mega espetáculo", "2018/04/30", "1:43:14" },
 };
 
-bool db::open_project (const std::string& project_name, void* system_context)
+bool dash::open_project (const std::string& project_name, void* system_context)
 {
 	return run_project (project_name, system_context);
 }
 
-bool db::delete_project (const std::string& project_name) { return false; }
-bool db::export_project (const std::string& project_name, const std::string& file_path) { return false; }
+bool dash::delete_project (const std::string& project_name) { return false; }
+bool dash::export_project (const std::string& project_name, const std::string& file_path) { return false; }
 
-bool db::rename_project (const std::string& old_name, const std::string& new_name)
+bool dash::rename_project (const std::string& old_name, const std::string& new_name)
 {
 	auto itr = std::find_if (projects.begin(), projects.end(), [&](const auto& project) { return project.name == old_name; });
 	if (itr != projects.end()) {
@@ -33,43 +33,43 @@ bool db::rename_project (const std::string& old_name, const std::string& new_nam
 		return false;
 }
 
-bool db::open_new_project (void* system_context)
+bool dash::open_new_project (void* system_context)
 {
 	std::string new_name = petipa::util::get_new_name (
 			projects,
 			"Project",
 			[](const auto& e){ return e.name; });
 
-	return db::open_project (new_name, system_context);
+	return dash::open_project (new_name, system_context);
 }
 
-bool db::open_project_file (const std::string& file_path, void* system_context)
+bool dash::open_project_file (const std::string& file_path, void* system_context)
 {
 	//TODO open project (to read the project name)
 	//TODO check for duplicate project name
 	//TODO copy project to data dir
-	//TODO return db::open_project (project_name, system_context);
+	//TODO return dash::open_project (project_name, system_context);
 	return false;
 }
 
-bool db::copy_and_open_project (const std::string& project_name, void* system_context)
+bool dash::copy_and_open_project (const std::string& project_name, void* system_context)
 {
 	//TODO
 	return false;
 }
 
-bool db::import_and_open_project (const std::string& file_path, void* system_context)
+bool dash::import_and_open_project (const std::string& file_path, void* system_context)
 {
 	//TODO
 	return false;
 }
 
-std::vector<db::ProjectListSection> db::get_project_list_sections()
+std::vector<dash::ProjectListSection> dash::get_project_list_sections()
 {
 	//TODO
-	std::vector<db::ProjectListSection> sections;
+	std::vector<dash::ProjectListSection> sections;
 	if (petipa::api::config::get_dashboard_alphabetic_sort()) {
-		db::ProjectListSection s1;
+		dash::ProjectListSection s1;
 		s1.label = "todos";
 		s1.projects.push_back(projects[1]);
 		s1.projects.push_back(projects[3]);
@@ -78,7 +78,7 @@ std::vector<db::ProjectListSection> db::get_project_list_sections()
 		sections.push_back(s1);
 	}
 	else {
-		db::ProjectListSection s1, s2;
+		dash::ProjectListSection s1, s2;
 		s1.label = "hoje";
 		s1.projects.push_back(projects[0]);
 		s2.label = "última semana";
@@ -92,18 +92,18 @@ std::vector<db::ProjectListSection> db::get_project_list_sections()
 	return sections;
 }
 
-std::string db::project_name_to_file_name (const std::string& project_name)
+std::string dash::project_name_to_file_name (const std::string& project_name)
 {
 	//TODO
 	return "/some/file/name.petipa";
 }
 
-std::string db::get_project_file_extension()
+std::string dash::get_project_file_extension()
 {
 	return "petipa";
 }
 
-std::string db::get_new_name (const std::string& project_name)
+std::string dash::get_new_name (const std::string& project_name)
 {
 	return petipa::util::get_new_name (
 			projects,
