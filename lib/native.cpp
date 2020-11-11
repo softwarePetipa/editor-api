06f60c9a8c888b6325683d5b97d204e717a91d7c
Rodolfo Borges <rodolfo.borges@gmail.com>
2020-11-11 17:39:07 -0300

    non-silent dummy native methods

--- lib/native.cpp
+++ lib/native.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include "../include/native.h"
 
-void petipa::api::native::open_visualization_options_dialog() {}
-void petipa::api::native::open_characters_dialog (const std::string& character_name) {}
-void petipa::api::native::open_music_dialog() {}
-void petipa::api::native::open_stage_dialog() {}
+void petipa::api::native::open_visualization_options_dialog()
+{
+	std::cout << "open visualization dialog\n";
+}
+
+void petipa::api::native::open_characters_dialog (const std::string& character_name)
+{
+	std::cout << "open characters dialog\n";
+}
+
+void petipa::api::native::open_music_dialog()
+{
+	std::cout << "open music dialog\n";
+}
+
+void petipa::api::native::open_stage_dialog()
+{
+	std::cout << "open stage dialog\n";
+}
+
 void petipa::api::native::open_notifications_dialog() {}
 void petipa::api::native::open_about_dialog() {}
 
@@ -13,7 +29,24 @@ void petipa::api::native::alert (const std::string& title, const std::string& te
 	std::cout << "alert(" << title << ")(" << text << ")\n";
 }
 
-bool petipa::api::native::confirm (const std::string& title, const std::string& text) { return false; }
-std::string petipa::api::native::text_input (const std::string& title) { return ""; }
-void petipa::api::native::share_project (const std::string& file_path) {}
-void petipa::api::native::open_url (const std::string& url) {}
+bool petipa::api::native::confirm (const std::string& title, const std::string& text)
+{
+	std::cout << "confirm(" << title << ")(" << text << ")\n";
+	return false;
+}
+
+std::string petipa::api::native::text_input (const std::string& title)
+{
+	std::cout << "input(" << title << ")\n";
+	return "";
+}
+
+void petipa::api::native::share_project (const std::string& file_path)
+{
+	std::cout << "share(" << file_path << ")\n";
+}
+
+void petipa::api::native::open_url (const std::string& url)
+{
+	std::cout << "open_url(" << url << ")\n";
+}
